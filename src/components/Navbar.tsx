6bac7481c981279f0d0f6d34efa42fc3903a264b
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-12-18 10:38:25 -0300

    Adicionando responsividade na navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,127 @@
+import React, { useState, useEffect } from 'react';
+
 interface Props {
   page: string;
 }
 
 export const Navbar: React.FC<Props> = ({ page }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="flex fixed w-full content-start flex-wrap text-slate-50 bg-neutral-900 bg-opacity-70 z-20">
-      <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
-        {page === 'sobre-mim' ? <a className="text-amber-300"href="http://localhost:5173/SobreMim">Sobre Mim</a> : <a href="http://localhost:5173/SobreMim">Sobre Mim</a>}
-      </div>
-      <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
-        {page === 'habilidades' ? <a className="text-amber-300"href="http://localhost:5173/Habilidades">Habilidades</a> : <a href="http://localhost:5173/Habilidades">Habilidades</a>}
-      </div>
-      <div className="flex-1 h-16 flex items-center justify-center">
-        <img src="/timonIcon.png" alt="Timon" height="70px" width="70px" />
-      </div>
-      <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
-        {page === 'projetos' ? <a className="text-amber-300"href="http://localhost:5173/Projetos">Projetos</a> : <a href="http://localhost:5173/Projetos">Projetos</a>}
-      </div>
-      <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
-        {page === 'contato' ? <a className="text-amber-300"href="http://localhost:5173/Contato">Contato</a> : <a href="http://localhost:5173/Contato">Contato</a>}
-      </div>
+    <div className={`flex fixed w-full content-start flex-wrap text-slate-50 bg-neutral-900 bg-opacity-70 z-20 ${isMobile ? 'md:hidden' : ''}`}>
+      {!isMobile && (
+        <>
+          <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
+            {page === 'sobre-mim' ? (
+              <a className="text-amber-300" href="http://localhost:5173/SobreMim">
+                Sobre Mim
+              </a>
+            ) : (
+              <a href="http://localhost:5173/SobreMim">Sobre Mim</a>
+            )}
+          </div>
+          <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
+            {page === 'habilidades' ? (
+              <a className="text-amber-300" href="http://localhost:5173/Habilidades">
+                Habilidades
+              </a>
+            ) : (
+              <a href="http://localhost:5173/Habilidades">Habilidades</a>
+            )}
+          </div>
+          <div className="flex-1 h-16 flex items-center justify-center">
+            <img src="/timonIcon.png" alt="Timon" height="70px" width="70px" />
+          </div>
+          <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
+            {page === 'projetos' ? (
+              <a className="text-amber-300" href="http://localhost:5173/Projetos">
+                Projetos
+              </a>
+            ) : (
+              <a href="http://localhost:5173/Projetos">Projetos</a>
+            )}
+          </div>
+          <div className="flex-1 h-16 flex items-center justify-center text-2xl font-itim hover:text-amber-300">
+            {page === 'contato' ? (
+              <a className="text-amber-300" href="http://localhost:5173/Contato">
+                Contato
+              </a>
+            ) : (
+              <a href="http://localhost:5173/Contato">Contato</a>
+            )}
+          </div>
+        </>
+      )}
+      {isMobile && (
+        <>
+         <div className="flex-1 h-16 flex items-center justify-center text-white text-2xl font-itim">
+            <p>Portfolio</p>
+          </div>
+          <div className="flex-1 h-16 flex items-center justify-center">
+            <img src="/timonIcon.png" alt="Timon" height="70px" width="70px" />
+          </div>
+          <div className="flex-1 h-16 flex items-center justify-center md:hidden">
+            <button className="text-white text-2xl font-itim hover:text-amber-300" onClick={toggleMenu}>Menu</button>
+          </div>
+          {menuOpen && (
+            <div className="md:hidden w-full">
+              <div className="text-white text-2xl font-itim hover:text-amber-300">
+                  {page === 'sobre-mim' ? (
+                  <a className="text-amber-300" href="http://localhost:5173/SobreMim">
+                    Sobre Mim
+                  </a>
+                ) : (
+                  <a href="http://localhost:5173/SobreMim">Sobre Mim</a>
+                )}
+              </div>
+              <div className="text-white text-2xl font-itim hover:text-amber-300">
+                {page === 'habilidades' ? (
+                  <a className="text-amber-300" href="http://localhost:5173/Habilidades">
+                    Habilidades
+                  </a>
+                ) : (
+                  <a href="http://localhost:5173/Habilidades">Habilidades</a>
+                )}
+              </div>
+              <div className="text-white text-2xl font-itim hover:text-amber-300">
+                {page === 'projetos' ? (
+                  <a className="text-amber-300" href="http://localhost:5173/Projetos">
+                    Projetos
+                  </a>
+                ) : (
+                  <a href="http://localhost:5173/Projetos">Projetos</a>
+                )}
+              </div>
+              <div className="text-white text-2xl font-itim hover:text-amber-300">
+                {page === 'contato' ? (
+                  <a className="text-amber-300" href="http://localhost:5173/Contato">
+                    Contato
+                  </a>
+                ) : (
+                  <a href="http://localhost:5173/Contato">Contato</a>
+                )}
+              </div>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
-}
+};
