7cfdce0bd6e622be558763cbe973fa811e7701f3
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-12-19 18:31:47 -0300

    Teste

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import {Rotas} from "./Routes.tsx"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <Rotas />
   </BrowserRouter>,
 )
