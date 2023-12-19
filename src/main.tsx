3760e59eb6418c1c8dd107f6145d9aa5affaa95a
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-12-19 18:36:47 -0300

    Testando

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
+import { Route } from 'react-router-dom';
+import {SobreMim} from "./pages/SobreMim.tsx";
+
 import {Rotas} from "./Routes.tsx"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename={window.location.pathname || ''}>
-    <Rotas />
-  </BrowserRouter>,
+  <Route path="/" element={<SobreMim />} />
+</BrowserRouter>,
 )
