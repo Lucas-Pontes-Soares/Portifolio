f579dde2037e6668ab40c3639557a63c07aee40d
Lucas-Pontes-Soares <lucas.soares177@etec.sp.gov.br>
2023-12-19 19:10:03 -0300

    T

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter } from "react-router-dom";
 import { Route } from 'react-router-dom';
 import {SobreMim} from "./pages/SobreMim.tsx";
 
-import {Rotas} from "./Routes.tsx"
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename={window.location.pathname || ''}>
-  <Route path="/" element={<SobreMim />} />
-</BrowserRouter>,
+    <Route path="/" element={<SobreMim />} />
+  </BrowserRouter>,
 )
